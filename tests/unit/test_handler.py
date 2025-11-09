de18a4624e6b90df0312f3a20224e134a724f4ce
Marcus Henry <marcus@Macmini.cg.shawcable.net>
2025-11-09 00:10:18 -0700

    Add simple pre-deploy test

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,7 +1,15 @@
-# tests/test_basic.py
+from app import lambda_handler
 
-def test_basic_math():
-    assert 2 + 2 == 4
+def test_options_request_returns_cors_headers():
+    # Arrange
+    event = {"httpMethod": "OPTIONS"}
+    context = {}
 
+    # Act
+    response = lambda_handler(event, context)
 
-
+    # Assert
+    assert response["statusCode"] == 200
+    assert "Access-Control-Allow-Origin" in response["headers"]
+    assert response["headers"]["Access-Control-Allow-Methods"] == "GET,OPTIONS"
+    assert response["body"] == ""
