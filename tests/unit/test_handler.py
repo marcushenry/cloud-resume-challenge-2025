fe13be7147f6b1753a9ea6e1b8a81fd557d18c2a
Marcus Henry <marcus@Macmini.cg.shawcable.net>
2025-11-08 20:38:34 -0700

    Add automated tests step

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,15 +1,14 @@
 # tests/unit/test_handler.py
 import json
-from moto import mock_s3
+from moto import mock_aws
 import boto3
 from hello_world import app  # your handler module
 
-@mock_s3
+@mock_aws
 def test_lambda_returns_200():
     # Arrange fake AWS
     s3 = boto3.client("s3", region_name="us-east-1")
     s3.create_bucket(Bucket="my-bucket")
-    # (put any needed objects here)
 
     # Act
     event = {"httpMethod": "GET"}
@@ -17,4 +16,5 @@ def test_lambda_returns_200():
 
     # Assert
     assert result["statusCode"] == 200
+
 
